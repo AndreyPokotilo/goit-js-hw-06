f87e1019af7a96cf49eef311ac19e99a91833413
AndreyPokotilo <102250174+AndreyPokotilo@users.noreply.github.com>
2022-12-04 17:44:26 +0100

    added second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredient = document.querySelector('#ingredients')
+
+ingredients.map(element => {
+  const li = document.createElement('li');
+  li.classList.add('item')
+  li.textContent = element
+  ingredient.appendChild(li)
+})
+
